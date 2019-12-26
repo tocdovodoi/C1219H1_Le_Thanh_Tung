ab9e8ae0686f54f0705d733f161e835ca831fd66
tocdovodoi <59156003+tocdovodoi@users.noreply.github.com>
2019-12-26 11:06:48 +0700

    vs4

---
+++ B2_BienKieuDuLieuToanTu/ThucHanh/KhaiBaoSuDungBien/Bai3/MyScript.js
@@ -0,0 +1,8 @@
+let a = parseInt(prompt("Nhập số a"));
+let b = parseInt(prompt("Nhập số b"));
+if(a%b===0){
+document.write("a là bội số của b");
+} else
+{
+    document.write("a không phải bội số của b");
+}
