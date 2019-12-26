f22c73b125261659a05ca5aee6e228896f9fbd22
tocdovodoi <59156003+tocdovodoi@users.noreply.github.com>
2019-12-26 11:21:17 +0700

    vs4

---
+++ B2_BienKieuDuLieuToanTu/ThucHanh/SuDungToanTu/MyScript.js
@@ -0,0 +1,5 @@
+let height = parseInt(prompt("Nhập chiều dài hình nhữ nhật"));
+let width = parseInt(prompt("Nhập chiều rộng hình nhữ nhật"));
+document.write("Chiều dài: " + height+ "<br>");
+document.write("Chiều rộng: " + width+ "<br>");
+document.write("<p style='color: red'>Diện tích hình chữ nhật bằng: " + height * width+ "</p>");
