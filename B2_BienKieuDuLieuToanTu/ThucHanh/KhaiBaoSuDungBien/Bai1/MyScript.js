3794b5ffa5bd437e0e5cdc46096afcef62c7d41d
tocdovodoi <59156003+tocdovodoi@users.noreply.github.com>
2019-12-26 10:58:01 +0700

    vs4

---
+++ B2_BienKieuDuLieuToanTu/ThucHanh/KhaiBaoSuDungBien/Bai1/MyScript.js
@@ -0,0 +1,8 @@
+let i = 10;
+const f = 20.5;
+let b = true;
+let s = "Hà Nội";
+document.write('i =  ' + i+ "<br>");
+document.write('f =  ' + f+ "<br>");
+document.write('b =  ' + b+ "<br>");
+document.write('s =  ' + s+ "<br>");
---
+++ B2_BienKieuDuLieuToanTu/ThucHanh/KhaiBaoSuDungBien/Bai2/MyScript.js
@@ -0,0 +1,4 @@
+let height = 5;
+let width = 10;
+let area = height*width;
+document.write('Diện tích hình chữ nhật bằng :' + area);
