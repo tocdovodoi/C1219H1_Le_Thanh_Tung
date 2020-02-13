68cd6231e63e9f4373f1b5f96008b40e2835eaf7
Lê Thanh Tùng <thanhtungspt@gmail.com>
2020-02-13 10:11:33 +0700

    13.02.2020(10h11)

---
+++ Module2/JavaCore/src/ThucHanh/KhaiBaoVaSuDungBien.java
@@ -0,0 +1,18 @@
+package ThucHanh;
+
+public class KhaiBaoVaSuDungBien {
+    public static void main(String[] args) {
+//        Biến i kiểu số nguyên, có giá trị là 10
+        int i = 10;
+//        Biến f kiểu số thực, có giá trị là 20.5
+        float f = 20.5f;
+//        Biến d kiểu số thực lớn, có giá trị là 20.5
+        double d = 20.5;
+//        Biến b kiểu logic, có giá trị là true
+        boolean b = true;
+//        Biến c kiểu ký tự, có giá trị là 'a'
+        char c = 'a';
+//        Biến s kiểu chuỗi, có giá trị là "Hà Nội".
+        String s = "Hà Nội";
+    }
+}
---
+++ Module2/JavaCore/src/ThucHanh/SystemTime.java
@@ -0,0 +1,10 @@
+package ThucHanh;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Thời gian hiện tại:" + now);
+    }
+}
